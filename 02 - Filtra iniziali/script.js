46267f84b2298de604f9ef9f96a360129fbd75a6
gennaroderosa10 <gennaroderosa5@gmail.com>
2025-11-04 16:29:32 +0100

    feat: 2 done

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,28 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function findA(array, letter) {
+    const iniziali = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i][0] === letter) {
+            iniziali.push(array[i])
 
+        }
+
+    }
+    return iniziali
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+const result = findA(names, "A")
+console.log(result);
 
+const result2 = findA(names, "M")
+console.log(result2);
+
+const result3 = findA(names, "L")
+console.log(result3);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]

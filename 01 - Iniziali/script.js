3d87d7b5d106d36ef6a5a5e2de8af7fcbffbdafd
gennaroderosa10 <gennaroderosa5@gmail.com>
2025-11-04 16:16:53 +0100

    feat: 1 done

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -2,12 +2,26 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
+//prova con altro array
+const names2 = ["Salvatore", "Antonio", "Gennaro"];
 
 // Dichiara la funzione qui.
+function findIniziali(array) {
+    const iniziali = [];
+    for (let i = 0; i < array.length; i++) {
+        iniziali.push(array[i][0]);
 
+    }
+    return iniziali
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+const result = findIniziali(names)
+console.log(result);
 
-
+//prova con altro array
+const result2 = findIniziali(names2)
+console.log(result2);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]

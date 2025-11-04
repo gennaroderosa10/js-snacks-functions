903d43021497e572ef83ab7879cdde72fc594228
gennaroderosa10 <gennaroderosa5@gmail.com>
2025-11-04 16:57:41 +0100

    feat: 3 done

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,29 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function findLetter(stringa) {
+    const vocali = ["a", "e", "i", "o", "u"];
+    let conto = 0;
+
+    for (let i = 0; i < stringa.length; i++) {
+        let stringIndx = stringa[i];
+
+        for (let y = 0; y < vocali.length; y++) {
+            if (stringIndx === vocali[y]) {
+                conto++
+            }
+        }
+
+    }
+    return conto;
+
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const result = findLetter(word);
+console.log(result);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
